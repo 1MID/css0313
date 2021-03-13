6ecaa846022a0582bd74cab95664ba36444807e0
Ray <LT1stSoloMID@gmail.com>
2021-03-13 10:36:52 +0800

    draft

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import './Reset.css';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          yaya 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header"> 
+      
+      <div class="banner">
+          <div class="container">
+              <div class="banner-txt">
+                  <h1>
+                      CSS切版練習
+                      <small> 
+                        好華好嫩
+                      </small>
+                  </h1>
+                  <h2>圖文滿版區塊 </h2>
+                  <p>也格炸彈超爽口感也格炸彈超爽口感也格炸彈超爽口感</p>
+              </div>
+          </div>
+      </div>
+
+
       </header>
     </div>
   );
